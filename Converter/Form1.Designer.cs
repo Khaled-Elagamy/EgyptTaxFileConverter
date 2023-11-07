df4072a03e1072f6b454443ff9d73354f9a9ce82
Khaled Elagamy <khaled_yasser1234@yahoo.com>
2023-11-07 08:10:57 +0300

    Create Auto-Update Functionality

--- Converter/Form1.Designer.cs
+++ Converter/Form1.Designer.cs
@@ -1,172 +1,173 @@
 ﻿namespace Converter
 {
-    partial class Form1
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            Title = new Label();
-            browseButton = new Button();
-            inputPath = new TextBox();
-            ConvertButton = new Button();
-            savePath = new TextBox();
-            openFileDialog = new OpenFileDialog();
-            saveFileDialog = new SaveFileDialog();
-            label1 = new Label();
-            label2 = new Label();
-            template_html = new OpenFileDialog();
-            SuspendLayout();
-            // 
-            // Title
-            // 
-            Title.AutoSize = true;
-            Title.BackColor = SystemColors.ActiveCaptionText;
-            Title.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            Title.Location = new Point(28, 9);
-            Title.Margin = new Padding(4, 0, 4, 0);
-            Title.Name = "Title";
-            Title.Size = new Size(142, 25);
-            Title.TabIndex = 0;
-            Title.Text = "Enter Json File";
-            // 
-            // browseButton
-            // 
-            browseButton.Location = new Point(425, 94);
-            browseButton.Margin = new Padding(4, 3, 4, 3);
-            browseButton.Name = "browseButton";
-            browseButton.Size = new Size(148, 27);
-            browseButton.TabIndex = 1;
-            browseButton.Text = "browse";
-            browseButton.UseVisualStyleBackColor = true;
-            browseButton.Click += browseButton_Click;
-            // 
-            // inputPath
-            // 
-            inputPath.BackColor = SystemColors.Control;
-            inputPath.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            inputPath.ForeColor = Color.DimGray;
-            inputPath.Location = new Point(28, 65);
-            inputPath.Margin = new Padding(4, 3, 4, 3);
-            inputPath.Name = "inputPath";
-            inputPath.ReadOnly = true;
-            inputPath.Size = new Size(345, 25);
-            inputPath.TabIndex = 4;
-            inputPath.Text = "Filepath...";
-            // 
-            // ConvertButton
-            // 
-            ConvertButton.Cursor = Cursors.Hand;
-            ConvertButton.ForeColor = SystemColors.Highlight;
-            ConvertButton.Location = new Point(47, 262);
-            ConvertButton.Margin = new Padding(4, 3, 4, 3);
-            ConvertButton.Name = "ConvertButton";
-            ConvertButton.Size = new Size(160, 27);
-            ConvertButton.TabIndex = 3;
-            ConvertButton.Text = "Convert";
-            ConvertButton.UseVisualStyleBackColor = true;
-            ConvertButton.Click += ConvertButton_Click;
-            // 
-            // savePath
-            // 
-            savePath.BackColor = SystemColors.Control;
-            savePath.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            savePath.ForeColor = Color.DimGray;
-            savePath.Location = new Point(28, 96);
-            savePath.Margin = new Padding(4, 3, 4, 3);
-            savePath.Name = "savePath";
-            savePath.ReadOnly = true;
-            savePath.Size = new Size(345, 25);
-            savePath.TabIndex = 4;
-            savePath.Text = "SaveLocation...";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = SystemColors.ActiveCaptionText;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(28, 159);
-            label1.Name = "label1";
-            label1.Size = new Size(360, 21);
-            label1.TabIndex = 5;
-            label1.Text = "The Pdf file will be created using the Template html";
-            label1.Visible = false;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = SystemColors.ActiveCaptionText;
-            label2.Cursor = Cursors.Hand;
-            label2.FlatStyle = FlatStyle.Popup;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(28, 195);
-            label2.Name = "label2";
-            label2.Size = new Size(237, 21);
-            label2.TabIndex = 6;
-            label2.Text = "To Choose Custom one Clickhere";
-            label2.Visible = false;
-            label2.Click += label2_Click;
-            // 
-            // template_html
-            // 
-            template_html.InitialDirectory = " Environment.CurrentDirectory";
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(614, 361);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(savePath);
-            Controls.Add(ConvertButton);
-            Controls.Add(inputPath);
-            Controls.Add(browseButton);
-            Controls.Add(Title);
-            Margin = new Padding(4, 3, 4, 3);
-            MaximumSize = new Size(900, 900);
-            MinimumSize = new Size(630, 400);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			Title = new Label();
+			browseButton = new Button();
+			inputPath = new TextBox();
+			ConvertButton = new Button();
+			savePath = new TextBox();
+			openFileDialog = new OpenFileDialog();
+			saveFileDialog = new SaveFileDialog();
+			label1 = new Label();
+			label2 = new Label();
+			template_html = new OpenFileDialog();
+			SuspendLayout();
+			// 
+			// Title
+			// 
+			Title.AutoSize = true;
+			Title.BackColor = SystemColors.ActiveCaptionText;
+			Title.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point);
+			Title.Location = new Point(28, 9);
+			Title.Margin = new Padding(4, 0, 4, 0);
+			Title.Name = "Title";
+			Title.Size = new Size(142, 25);
+			Title.TabIndex = 0;
+			Title.Text = "Enter Json File";
+			// 
+			// browseButton
+			// 
+			browseButton.Location = new Point(425, 94);
+			browseButton.Margin = new Padding(4, 3, 4, 3);
+			browseButton.Name = "browseButton";
+			browseButton.Size = new Size(148, 27);
+			browseButton.TabIndex = 1;
+			browseButton.Text = "browse";
+			browseButton.UseVisualStyleBackColor = true;
+			browseButton.Click += browseButton_Click;
+			// 
+			// inputPath
+			// 
+			inputPath.BackColor = SystemColors.Control;
+			inputPath.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+			inputPath.ForeColor = Color.DimGray;
+			inputPath.Location = new Point(28, 65);
+			inputPath.Margin = new Padding(4, 3, 4, 3);
+			inputPath.Name = "inputPath";
+			inputPath.ReadOnly = true;
+			inputPath.Size = new Size(345, 25);
+			inputPath.TabIndex = 4;
+			inputPath.Text = "Filepath...";
+			// 
+			// ConvertButton
+			// 
+			ConvertButton.Cursor = Cursors.Hand;
+			ConvertButton.ForeColor = SystemColors.Highlight;
+			ConvertButton.Location = new Point(47, 262);
+			ConvertButton.Margin = new Padding(4, 3, 4, 3);
+			ConvertButton.Name = "ConvertButton";
+			ConvertButton.Size = new Size(160, 27);
+			ConvertButton.TabIndex = 3;
+			ConvertButton.Text = "Convert";
+			ConvertButton.UseVisualStyleBackColor = true;
+			ConvertButton.Click += ConvertButton_Click;
+			// 
+			// savePath
+			// 
+			savePath.BackColor = SystemColors.Control;
+			savePath.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+			savePath.ForeColor = Color.DimGray;
+			savePath.Location = new Point(28, 96);
+			savePath.Margin = new Padding(4, 3, 4, 3);
+			savePath.Name = "savePath";
+			savePath.ReadOnly = true;
+			savePath.Size = new Size(345, 25);
+			savePath.TabIndex = 4;
+			savePath.Text = "SaveLocation...";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.BackColor = SystemColors.ActiveCaptionText;
+			label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			label1.Location = new Point(28, 159);
+			label1.Name = "label1";
+			label1.Size = new Size(360, 21);
+			label1.TabIndex = 5;
+			label1.Text = "The Pdf file will be created using the Template html";
+			label1.Visible = false;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.BackColor = SystemColors.ActiveCaptionText;
+			label2.Cursor = Cursors.Hand;
+			label2.FlatStyle = FlatStyle.Popup;
+			label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			label2.Location = new Point(28, 195);
+			label2.Name = "label2";
+			label2.Size = new Size(237, 21);
+			label2.TabIndex = 6;
+			label2.Text = "To Choose Custom one Clickhere";
+			label2.Visible = false;
+			label2.Click += label2_Click;
+			// 
+			// template_html
+			// 
+			template_html.InitialDirectory = " Environment.CurrentDirectory";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.ActiveCaption;
+			ClientSize = new Size(614, 361);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(savePath);
+			Controls.Add(ConvertButton);
+			Controls.Add(inputPath);
+			Controls.Add(browseButton);
+			Controls.Add(Title);
+			Margin = new Padding(4, 3, 4, 3);
+			MaximumSize = new Size(900, 900);
+			MinimumSize = new Size(630, 400);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private Label Title;
-        private Button browseButton;
-        private TextBox inputPath;
-        private Button ConvertButton;
-        private TextBox savePath;
-        private OpenFileDialog openFileDialog;
-        private SaveFileDialog saveFileDialog;
-        private Label label1;
-        private Label label2;
-        private OpenFileDialog template_html;
-    }
+		private Label Title;
+		private Button browseButton;
+		private TextBox inputPath;
+		private Button ConvertButton;
+		private TextBox savePath;
+		private OpenFileDialog openFileDialog;
+		private SaveFileDialog saveFileDialog;
+		private Label label1;
+		private Label label2;
+		private OpenFileDialog template_html;
+	}
 }
 
--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -1,112 +1,126 @@
 ﻿using Newtonsoft.Json;
 using Nustache.Core;
+using Squirrel;
 using System.Dynamic;
 using System.Reflection;
 using System.Text;
 namespace Converter
 {
-    public partial class Form1 : Form
-    {
-        private string template_path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\template.html";
+	public partial class Form1 : Form
+	{
+		private string template_path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\template.html";
+		UpdateManager manager;
+		public Form1()
+		{
+			InitializeComponent();
+			Load += Form1_Load;
+		}
+		private async void Form1_Load()
+		{
+			manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/Khaled-Elagamy/EgyptTaxFileConverter");
+			var updateinfo = await manager.CheckForUpdate();
+			this.Text += manager.CurrentlyInstalledVersion().ToString();
+			if (updateinfo.ReleasesToApply.Count > 0)
+			{
+				label1.Text = "New update Avaliable \nThe app will be Updated after restart";
+				label1.Show();
+				await manager.UpdateApp();
+			}
+		}
+		private void browseButton_Click(object sender, EventArgs e)
+		{
+			openFileDialog.Filter = "JSON Files (*.json)|*.json|All files (*.*)|*.*";
+			//openFileDialog.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml";
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				inputPath.Text = openFileDialog.FileName;
+				label1.Text = "The Pdf file will be created using the Template html";
+				label1.ForeColor = SystemColors.ControlText;
+				label1.BackColor = SystemColors.ActiveCaptionText;
+				label1.Show();
+				label2.Show();
+			}
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        private void browseButton_Click(object sender, EventArgs e)
-        {
-            openFileDialog.Filter = "JSON Files (*.json)|*.json|All files (*.*)|*.*";
-            //openFileDialog.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                inputPath.Text = openFileDialog.FileName;
-                label1.Text = "The Pdf file will be created using the Template html";
-                label1.ForeColor = SystemColors.ControlText;
-                label1.BackColor = SystemColors.ActiveCaptionText;
-                label1.Show();
-                label2.Show();
-            }
+		}
+		private void label2_Click(object sender, EventArgs e)
+		{
+			Get_html_file();
+		}
+		private void ConvertButton_Click(object sender, EventArgs e)
+		{
+			Check_Tempelate(template_path);
+			string directoryPath = Path.GetDirectoryName(openFileDialog.FileName);
+			using (saveFileDialog)
+			{
+				saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+				saveFileDialog.InitialDirectory = directoryPath;
 
-        }
-        private void label2_Click(object sender, EventArgs e)
-        {
-            Get_html_file();
-        }
-        private void ConvertButton_Click(object sender, EventArgs e)
-        {
-            Check_Tempelate(template_path);
-            string directoryPath = Path.GetDirectoryName(openFileDialog.FileName);
-            using (saveFileDialog)
-            {
-                saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
-                saveFileDialog.InitialDirectory = directoryPath;
+				if (saveFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					savePath.Text = saveFileDialog.FileName;
+				}
+				else { return; }
+			}
+			if (string.IsNullOrEmpty(openFileDialog.FileName) || string.IsNullOrEmpty(saveFileDialog.FileName))
+			{
+				MessageBox.Show("Please provide input Json and output PDF file paths.", "Error");
+				return;
+			}
+			else
+			{
+				DeserializeJson(inputPath.Text);
+			}
+		}
+		private void Check_Tempelate(string template_path)
+		{
+			if (template_html.FileName == "" && File.Exists(template_path))
+			{
+				template_html.FileName = template_path;
+			}
+			else if (!File.Exists(template_path))
+			{
+				MessageBox.Show("The HtmlTemplate file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				label1.Text = "Can't find file" + template_path;
+				label2.Text = "Click to Choose File";
+				Get_html_file();
+			}
 
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    savePath.Text = saveFileDialog.FileName;
-                }
-                else { return; }
-            }
-            if (string.IsNullOrEmpty(openFileDialog.FileName) || string.IsNullOrEmpty(saveFileDialog.FileName))
-            {
-                MessageBox.Show("Please provide input Json and output PDF file paths.", "Error");
-                return;
-            }
-            else
-            {
-                DeserializeJson(inputPath.Text);
-            }
-        }
-        private void Check_Tempelate(string template_path)
-        {
-            if (template_html.FileName == "" && File.Exists(template_path))
-            {
-                template_html.FileName = template_path;
-            }
-            else if (!File.Exists(template_path))
-            {
-                MessageBox.Show("The HtmlTemplate file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                label1.Text = "Can't find file" + template_path;
-                label2.Text = "Click to Choose File";
-                Get_html_file();
-            }
+		}
+		private void Get_html_file()
+		{
+			using (template_html)
+			{
+				template_html.Filter = "Html files (*.html)|*.html";
+				//openFileDialog.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml";
+				if (template_html.ShowDialog() == DialogResult.OK)
+				{
+					string selectedFileName = Path.GetFileName(template_html.FileName);
+					template_path = template_html.FileName;
+					label1.Text = "Creating Pdf File using " + selectedFileName;
+					label2.Text = "Click here to change the file";
+				}
+			}
+		}
+		private void DeserializeJson(string jsonFilePath)
+		{
+			string jsonString = File.ReadAllText(jsonFilePath, Encoding.UTF8);
+			InvoiceModel invoiceData = JsonConvert.DeserializeObject<InvoiceModel>(jsonString);
+			DocumentModel? document = JsonConvert.DeserializeObject<DocumentModel>(invoiceData.document);
+			invoiceData.Documents = document;
+			if (invoiceData != null && document != null)
+			{
+				htmltopdf(template_html.FileName, saveFileDialog.FileName, invoiceData, document);
+			}
+			else
+			{
+				MessageBox.Show("The File has Wrong Format.", "Error");
+				return;
+			}
+		}
 
-        }
-        private void Get_html_file()
-        {
-            using (template_html)
-            {
-                template_html.Filter = "Html files (*.html)|*.html";
-                //openFileDialog.Filter = "JSON Files (*.json)|*.json|XML Files (*.xml)|*.xml";
-                if (template_html.ShowDialog() == DialogResult.OK)
-                {
-                    string selectedFileName = Path.GetFileName(template_html.FileName);
-                    template_path = template_html.FileName;
-                    label1.Text = "Creating Pdf File using " + selectedFileName;
-                    label2.Text = "Click here to change the file";
-                }
-            }
-        }
-        private void DeserializeJson(string jsonFilePath)
-        {
-            string jsonString = File.ReadAllText(jsonFilePath, Encoding.UTF8);
-            InvoiceModel invoiceData = JsonConvert.DeserializeObject<InvoiceModel>(jsonString);
-            DocumentModel? document = JsonConvert.DeserializeObject<DocumentModel>(invoiceData.document);
-            invoiceData.Documents = document;
-            if (invoiceData != null && document != null)
-            {
-                htmltopdf(template_html.FileName, saveFileDialog.FileName, invoiceData, document);
-            }
-            else
-            {
-                MessageBox.Show("The File has Wrong Format.", "Error");
-                return;
-            }
-        }
-
-        private void htmltopdf(string templatePath, string outputPath, InvoiceModel invoice, DocumentModel document)
-        {
-            /*
+		private void htmltopdf(string templatePath, string outputPath, InvoiceModel invoice, DocumentModel document)
+		{
+			/*
             // JSON data for the invoice
             var invoiceData = new
             {
@@ -130,72 +144,77 @@ namespace Converter
                 // Add more data as needed
             };
             */
-            /*Create new file with data formatting*/
-            switch (invoice.status.ToLower())
-            {
-                case "submitted":
-                    invoice.status = "مُقَدَّم";
-                    break;
-                case "valid":
-                    invoice.status = "صحيح";
-                    break;
-                case "invalid":
-                    invoice.status = "غير صحيح";
-                    break;
-                case "rejected":
-                    invoice.status = "مرفوض";
-                    break;
-                case "cancelled":
-                    invoice.status = "ملغى";
-                    break;
-                default:
-                    invoice.status = "غير محدد";
-                    break;
-            }
-            dynamic invoiceData = new ExpandoObject();
-            invoiceData = invoice;
-            dynamic documentData = new ExpandoObject();
-            documentData = document;
-            //Create DTO with the data needed and the inovice object to create json file and other stuff
-            // Combine the data into a single dictionary
-            var combinedData = new Dictionary<string, object>
-            {
-                { "I", invoiceData },
-                { "D", documentData }
-            };
-            string filePath = "combinedData.txt"; // Specify the file path
+			/*Create new file with data formatting*/
+			switch (invoice.status.ToLower())
+			{
+				case "submitted":
+					invoice.status = "مُقَدَّم";
+					break;
+				case "valid":
+					invoice.status = "صحيح";
+					break;
+				case "invalid":
+					invoice.status = "غير صحيح";
+					break;
+				case "rejected":
+					invoice.status = "مرفوض";
+					break;
+				case "cancelled":
+					invoice.status = "ملغى";
+					break;
+				default:
+					invoice.status = "غير محدد";
+					break;
+			}
+			dynamic invoiceData = new ExpandoObject();
+			invoiceData = invoice;
+			dynamic documentData = new ExpandoObject();
+			documentData = document;
+			//Create DTO with the data needed and the inovice object to create json file and other stuff
+			// Combine the data into a single dictionary
+			var combinedData = new Dictionary<string, object>
+			{
+				{ "I", invoiceData },
+				{ "D", documentData }
+			};
+			string filePath = "combinedData.txt"; // Specify the file path
 
-            Type objType = invoiceData.GetType();
-            var properties = objType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+			Type objType = invoiceData.GetType();
+			var properties = objType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                foreach (var property in properties)
-                {
-                    var value = property.GetValue(invoiceData);
-                    writer.WriteLine($"{property.Name}: {value}");
-                }
-            }
-            try
-            {
-                string htmlTemplate = File.ReadAllText(templatePath);
-                string renderedHtml = Render.StringToString(htmlTemplate, combinedData);
-                PdfGenerator.GeneratePdf(renderedHtml, outputPath);
-                savePath.Clear();
-                saveFileDialog.FileName = null;
-                label1.Text = "File Created successfully";
-                label1.ForeColor = Color.Black;
-                label1.BackColor = Color.Green;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error reading the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+			using (StreamWriter writer = new StreamWriter(filePath))
+			{
+				foreach (var property in properties)
+				{
+					var value = property.GetValue(invoiceData);
+					writer.WriteLine($"{property.Name}: {value}");
+				}
+			}
+			try
+			{
+				string htmlTemplate = File.ReadAllText(templatePath);
+				string renderedHtml = Render.StringToString(htmlTemplate, combinedData);
+				PdfGenerator.GeneratePdf(renderedHtml, outputPath);
+				savePath.Clear();
+				saveFileDialog.FileName = null;
+				label1.Text = "File Created successfully";
+				label1.ForeColor = Color.Black;
+				label1.BackColor = Color.Green;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error reading the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
 
 
 
-        /*private void CreateJson()
+		/*private void CreateJson()
         {
             var combinedData = new ExpandoObject() as IDictionary<string, object>;
 
@@ -238,7 +257,7 @@ namespace Converter
             }
         }
         */
-        /*
+		/*
       try
       {
       CreatePdf(pdfFilePath, xmlFilePath);
@@ -252,7 +271,7 @@ namespace Converter
 
 
 
-        /*
+		/*
         private void CreatePdf(string pdfFilePath, string xmlFilePath)
         {
             Document doc = new Document();
@@ -299,7 +318,7 @@ namespace Converter
             }
         }
         */
-    }
+	}
 
 
 
